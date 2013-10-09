4ceef98e084b9cc2e245ad2aa21c5963dcd4442f
David Allen <davidrallen1@gmail.com>
2013-10-08 22:58:46 -0700

    Switched to using 1440 for midnight

--- lib/time_transformer.rb
+++ lib/time_transformer.rb
@@ -5,6 +5,7 @@ class TimeTransformer
     time_array = time_string.split(':')
     time = (time_array[0].to_i * 60) + time_array[1].to_i
     time += 720 if period == "PM" && time_string != "12:00"
+    time = 1440 if period == "AM" && time_string == "12:00"
     return time
   end
 
@@ -13,12 +14,11 @@ class TimeTransformer
     hours = ((time / 60).to_i % 12)
     hours = 12 if hours == 0
     time_string = hours.to_s + ':' + minutes
-    if time < 720
+    if time < 720 || time == 1440
       time_string += " AM" 
     else 
       time_string += " PM"
     end
-
     return time_string
   end
 
--- spec/lib/time_transformer_spec.rb
+++ spec/lib/time_transformer_spec.rb
@@ -6,7 +6,8 @@ describe TimeTransformer do
     TimeTransformer.str_to_i('2:30 PM').should eql(870)
     TimeTransformer.str_to_i('2:30 AM').should eql(150)
   end
-  it "handles noon correctly for str to i" do
+  it "handles midnight and noon correctly for str to i" do
+    TimeTransformer.str_to_i('12:00 AM').should eql(1440)
     TimeTransformer.str_to_i('12:00 PM').should eql(720)
   end
   it "can convert time from int to string" do
@@ -14,7 +15,7 @@ describe TimeTransformer do
     TimeTransformer.i_to_str(150).should eql('2:30 AM')
   end
   it "handles midnight and noon correctly for i to str" do
-    TimeTransformer.i_to_str(0).should eql('12:00 AM')
+    TimeTransformer.i_to_str(1440).should eql('12:00 AM')
     TimeTransformer.i_to_str(720).should eql('12:00 PM')
   end
 end
